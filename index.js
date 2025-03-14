d5319bb7c2876b26f78ffeb87f9bcd7b15fe1469
ZyAjahT <zyajah@gmail.com>
2025-03-13 23:17:41 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express'); 
 var app = express(); 
+const path = require('path');
+
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res){
-    req.send('New Node App');
+    res.render('index', {userName: req.query.userName || 'CPSC'});
+//  res.send({"key": "value"});
+})
+
+app.get('/name/:userName', function(req, res){
+    res.render('index', {userName: req.params.userName});
+//  res.send({"key": "value"});
 })
 
 app.listen(3000, function(){
